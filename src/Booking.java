63ff4b863553eeabf762d2de7f7caea9357a9a7c
Bourkupine <30806087+Bourkupine@users.noreply.github.com>
2022-10-18 12:48:01 +0100

    Added Booking class & renamed main file

---
+++ src/Booking.java
@@ -0,0 +1,4 @@
+//Euan: everytime a user creates a new booking, it will make an object here
+
+public class Booking {
+}
---
+++ src/RestaurantManager.java
@@ -0,0 +1,8 @@
+//Euan: this can act like the superclass
+
+public class RestaurantManager {
+    public static void main(String[] args) {
+
+
+    }
+}
