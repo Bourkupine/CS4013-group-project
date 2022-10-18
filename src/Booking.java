b8b5285f5fc8aa7fb0af9324c60648005c187376
Bourkupine <30806087+Bourkupine@users.noreply.github.com>
2022-10-18 12:57:07 +0100

    added constructors into Booking

--- src/Booking.java
+++ src/Booking.java
@@ -1,4 +1,21 @@
 //Euan: everytime a user creates a new booking, it will make an object here
+//I had the idea that there would be two options, one with email and phone number and one without for contacting.
 
 public class Booking {
+
+    private String name;
+    private String email;
+    private String phoneNumber; //NEEDS to be stored as a String as otherwise 083 will become 83
+    private int amountOfPeople;
+
+
+    public Booking(String name, int amountOfPeople) {
+        this.name = name;
+        this.amountOfPeople = amountOfPeople;
+    }
+    public Booking(String name, int amountOfPeople, String email, String phoneNumber) {
+        this(name, amountOfPeople);
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -1,4 +1,4 @@
-//Euan: this can act like the superclass
+//Euan: this can act like the main (overview?) class
 
 public class RestaurantManager {
     public static void main(String[] args) {
