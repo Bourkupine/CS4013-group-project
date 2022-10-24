ea8daed081e978fe6c104004843489719910955a
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-10-24 17:25:22 +0100

    updated order

---
+++ src/Order.java
@@ -0,0 +1,17 @@
+// Thomas : This is for the order 
+// i need to increment the loayalty badge fro the customer. 
+// i need to make a receipt with the customers details 
+
+
+public class Order{
+    
+    setLoyalty() + 1 ;
+
+
+    String receipt(){
+
+    }
+
+
+    
+}
