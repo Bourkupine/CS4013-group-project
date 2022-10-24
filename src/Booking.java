f67057d19c438c2099c81bc5d5e9e9b26c44137a
bourkupine <euanbourke@gmail.com>
2022-10-24 17:09:57 +0100

    added missing classes

--- src/Booking.java
+++ src/Booking.java
@@ -1,12 +1,10 @@
 //Euan: everytime a user creates a new booking, it will make an object here
 //I had the idea that there would be two options, one with email and phone number and one without for contacting.
 
-import java.time.LocalDateTime;
-import java.util.Formatter;
 
 public class Booking {
 
-
+    Customer customer = new Customer();
 
 
 }
