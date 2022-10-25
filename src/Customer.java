12a4d2464d90624941d6dd0dac4c6be3ba775e22
RLynch59 <ronanlynch95@gmail.com>
2022-10-25 14:44:55 +0100

    Added comments to customer methods

--- src/Customer.java
+++ src/Customer.java
@@ -4,15 +4,16 @@
  * @author Ronan
  */
 public class Customer {
-    private String name;
-    private int id;
-    private String phoneNumber;
-    private int loyalty=1;
+    private String name;//Customers name
+    private int id;//Unique id number for customer
+    private String phoneNumber;//Phone number of customer
+    private int loyalty=1;//number of times customer has been to restaurant
 
     /**
      * Non phone number constructor (ideal for walk-ins)
      * @param name name of customer
      * @param id id number for customer
+     * @author Ronan
      */
     public Customer(String name, int id){
         this.name=name;
@@ -24,6 +25,7 @@ public class Customer {
      * @param name name of customer
      * @param id id number for customer
      * @param phoneNumber phone number of customer
+     * @author Ronan
      */
     public Customer(String name, int id, String phoneNumber){
         this(name,id);
@@ -32,27 +34,54 @@ public class Customer {
 
     //Below are getters and setters as neccessary
 
+    /**
+     * Gets name of customer
+     * @return name as String
+     * @author Ronan
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets id number for customer
+     * @return id number as int
+     * @author Ronan
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets phone number of customer
+     * @return phone number as String
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Returns number of times customer has been to restaurant
+     * @return number of visits as int
+     */
     public int getLoyalty() {
         return loyalty;
     }
 
+    /**
+     * Sets number of times customer has been to restaurant
+     * @param loyalty number of visits
+     */
     public void setLoyalty(int loyalty) {
         this.loyalty = loyalty;
     }
 
     //Basic toString. Can modify if needed.
+
+    /**
+     *
+     * @return a String representation of the customer
+     */
     @Override
     public String toString(){
         return "Customer "+name+" (id "+id+") has made "+loyalty+" visits.";
