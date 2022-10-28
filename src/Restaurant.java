443096049776ba677131c80276048c71d21df83c
bourkupine <euanbourke@gmail.com>
2022-10-28 11:41:28 +0100

    added table generator

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,6 @@
 //Euan: this can act like the main (overview?) class
 
+
 import java.util.ArrayList;
 
 public class Restaurant {
@@ -8,15 +9,19 @@ public class Restaurant {
     private ArrayList<Customer> customers = new ArrayList<>();
     private ArrayList<Table> tables = new ArrayList<>();
 
-    public Restaurant(int tableNumber) { //Look back into this
+    public Restaurant(int amountOfTables) { //Look back into this
 
         //very temporary solution
-        for (int i = 0; i < tableNumber; i++) {
-            Table table = new Table();
+        for (int i = 0; i < amountOfTables; i++) {
+            Table table = new Table(i, (int) Math.pow(2, (i%3)+1)); //see comment below method for this
             tables.add(table);
         }
-
     }
+    //Math.pow(x, y) will return x^y, by using %3 we will return a number from 0-2 each time, we then +1.
+    //so what this is effectively giving us is, Math.pow(2, (1/2/3)) i.e 2^(1/2/3)
+    //where 2^(1/2/3) will cycle between the values in the bracket each time i is incremented
+    //thus giving us 2^1, 2^2, 2^3, or 2, 4, 8
+    //this allows us to generate tables of these sizes.
 
     //Getters and Setters
     public ArrayList<Customer> getCustomers() {
