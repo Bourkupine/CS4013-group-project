8ffb030ddc0dbdd62da20590afb10904195eb467
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-01 14:59:32 +0000

    minor changes typos etc

--- src/Order.java
+++ src/Order.java
@@ -8,7 +8,7 @@ import java.util.Formatter;
 import java.util.Scanner;
 
 public class Order{
-    private ArrayList<FoodItem> orderd = new ArrayList<>();
+    private ArrayList<FoodItem> ordered = new ArrayList<>();
     private Customer c; // bayan:
     private int total = 0; //thomas adding up the total amount for the bill
     private Menu m ;
@@ -30,16 +30,16 @@ public class Order{
         //t: creating the scanner
         Scanner order = new Scanner(System.in);
         
-        System.out.println(Restaurant.printMenu()); //t:this is printing the menu so the customer can choose their items
+        System.out.println(m.toString()); //t:this is printing the menu so the customer can choose their items
         boolean ordering = true ; //t: this is for the customers they will input 0 when they want to stop ordering 
         while(ordering ){
             // Menu item input
             int item = order.nextInt();
 
             //need to relate item number to whatever that no. is on the menu and put that in the array list... not just the no.
-            orderd.add(m.getFoodItemAtIndex(item)); //t adding item to the string for the bill
+            ordered.add(m.getFoodItemAtIndex(item)); //t adding item to the string for the bill
             System.out.println("This is your current order " + "\n"); //printing what the customer has currently ordered so the can see what their order is currently
-            System.out.println(orderd.toString()); // this is the array list of what they are currently ordering
+            System.out.println(ordered.toString()); // this is the array list of what they are currently ordering
             
             if (item == 0){ //t this is breaking out of the ordering when the customer decides
                 break;
@@ -65,13 +65,13 @@ public class Order{
     * {@summary Gets what customer c is }
     @author Thomas
     */
-        public Customer getC() {
+        public Customer getCustomer() {
             return c;
         }
         
         @Override
         //this definitely wont work but going to leave it here for now 
-        //also dont think i need to have a to string here maybe for bill to use but for now im not sure 
+        //also dont think i need to have a to string here maybe
         public String toString() {
             
             return orderd.toString();
