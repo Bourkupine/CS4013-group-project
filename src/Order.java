525fe2e539072262a0fb540ff246e9591bffa1d4
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-01 15:33:02 +0000

    no. changes

--- src/Order.java
+++ src/Order.java
@@ -32,19 +32,19 @@ public class Order{
         while(ordering ){
             // Menu item input
             int item = order.nextInt();
-            if (item == 14){
+            if (item == 0){
                 break;
-            } else if (item == 15){
+            } else if (item == 14){
                 int removeItem = order.nextInt();
                 ordered.remove(removeItem);
-            } else if( item >= 0 & item <= 13 ){
+            } else if( item > 0 & item <= 13 ){
                 
                 //need to relate item number to whatever that no. is on the menu and put that in the array list... not just the no.
                 ordered.add(m.getFoodItemAtIndex(item)); //t adding item to the string for the bill
                 System.out.println("This is your current order " + "\n"); //printing what the customer has currently ordered so the can see what their order is currently
                 System.out.println(ordered.toString()); // this is the array list of what they are currently ordering
-                System.out.println("To confirm your order enter the number 14");
-                System.out.println("To cancel an item enter the number 15 ");
+                System.out.println("To confirm your order enter the number 0");
+                System.out.println("To cancel an item enter the number 14 ");
             }
         }
         
