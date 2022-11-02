4078f579a50df134e95de184ddd38ef2cda1e0b8
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-02 11:52:48 +0000

    tried to fix enums made a new main folder

--- src/staff/Chef.java
+++ src/staff/Chef.java
@@ -1,43 +1,28 @@
 package staff;
-
+import Main.*;
 
 
 public class Chef extends Staff {
     
-    private FOODSTATUS status; // creating a variable to store ordered ready and delivered
     private Waiter waiter ;
+    private Order order ;
     
     public Chef(String name){
         super(name);
     }
     
-    public void setStatus(FOODSTATUS status) {
-        this.status = status;
-    }
-    
-    public enum FOODSTATUS { //assigning the enums to FOODSTATUS
-        WAITING,
-        ORDERED,
-        READY, 
-        DELIVERED; 
-    }
-    public void ordered(){
-        setStatus(FOODSTATUS.ORDERED);
-        
-    }
-    
     public void isDeliverable() { // this will tell the waiter wether the order is ready to be dropped yet or not
-        if (getStatus() == FOODSTATUS.READY) { //if the enum value is ready then the waiter will drop it 
+        if ( order.ge== Order.status.READY) { //if the enum value is ready then the waiter will drop it 
             waiter.bellRings(); // this tells the waiter the food is ready to be dropped by the chef ringing the bell
         }
     }
     
-    public FOODSTATUS getStatus() {
+    public status getStatus() {
         return status;
     }
     
     public void delivered(){ //tells the chef the food has been delivered
-        setStatus(FOODSTATUS.WAITING);
+        setStatus(status.WAITING);
     }
     
 }
