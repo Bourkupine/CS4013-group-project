0ea15a93c60f82fe965f51e196331865264dc80f
Bayan <b.nezamabad@gmail.com>
2022-11-04 14:20:57 +0000

    Multiple menu's can be created with updated toString() methods.

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,33 +1,21 @@
 //Bayan: A class to represent a food item at a restaurant
 
 public class FoodItem {
-    private int id;
     private String name;
     private double price;
     private String type;
 
     /**
      * Full-arg constructor
-     * @param id the id of the item
      * @param name the name of the item
      * @param price the item's price
      * @param type the type of the item (starter, main, etc.)
      * @author Bayan
      */
-    public FoodItem(int id, String name, double price, String type) {
-        this.id = id; //Bayan: the id number used to identify the food item
+    public FoodItem(String name, double price, String type) {
         this.name = name; //Bayan: the name of the food item
         this.price = price; //Bayan: the price of the food item
         this.type = type; //Bayan: the type of the food item (starter, main, dessert, or drink)
-    }
-
-    /**
-     * Gets the id of the food item
-     * @return id as int
-     * @author Bayan
-     */
-    public int getId() {
-        return id;
     }
 
     /**
@@ -55,5 +43,15 @@ public class FoodItem {
      */
     public String getType() {
         return type;
+    }
+
+    /**
+     * Returns a string representation of a FoodItem
+     * @return FoodItem as String
+     * @author Bayan
+     */
+    @Override
+    public String toString() {
+        return String.format("%-20s%5.2f%10s", name, price, type);
     }
 }
