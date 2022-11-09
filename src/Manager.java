1b7299650923c6c7540f8336b062ca2740100207
Bayan <b.nezamabad@gmail.com>
2022-11-09 10:46:44 +0000

    Added manager methods and started manager ui

--- src/Manager.java
+++ src/Manager.java
@@ -1,13 +1,16 @@
-
-
-
-
 public class Manager extends Staff {
 
     public Manager(String name,String password, Restaurant rest){
         super(name,password,rest);
     }
 
+    public void addToMenu(FoodItem food) {
+        getRest().getMenu().addFood(food);
+    }
+
+    public void removeFromMenu(FoodItem food) {
+        getRest().getMenu().removeFood(food);
+    }
 
     public void employStaff(Staff staff) {
         getRest().getStaff().add(staff);
@@ -16,9 +19,4 @@ public class Manager extends Staff {
     public void fireStaff(Staff staff) {
         getRest().getStaff().remove(staff);
     }
-
-
-
-
-
 }
