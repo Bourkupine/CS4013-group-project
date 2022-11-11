4f80b930505587a823f2aae691f6e8004102bad9
RLynch59 <ronanlynch95@gmail.com>
2022-11-11 14:32:51 +0000

    Added RestaurantSimulation

---
+++ src/RestaurantSimulation.java
@@ -0,0 +1,9 @@
+/**
+ * This is the class we will run to start the program
+ */
+public class RestaurantSimulation {
+
+    public static void main(String[] args) {
+        RestaurantChain rc = new RestaurantChain("Yum's",15);
+    }
+}
