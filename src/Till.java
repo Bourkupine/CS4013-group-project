5bf2447c61a37f2f6fb53c1d960bdfab66abd72b
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-14 11:21:29 +0000

    till to string fixing

--- src/Till.java
+++ src/Till.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 //builds bill and a receipt 
 //
 /**
- * this is the till class
- * @author Thomas
- */
+* this is the till class
+* @author Thomas
+*/
 public class Till {
     Scanner in = new Scanner(System.in);
+    private double change = 0;
     private Restaurant rest;
     private boolean cash;
     private double amountDue;
@@ -17,93 +18,103 @@ public class Till {
     private double cashInDrawer;
     private int amountGiven;
     private Customer c;
-/**
- * 
- * @param rest this is the restaurant the till is in
- * @param amountDue this is how much the order came to
- * @param c this is the customer that owes us money grrr
- * @author Thomas
- */
+    /**
+    * 
+    * @param rest this is the restaurant the till is in
+    * @param amountDue this is how much the order came to
+    * @param c this is the customer that owes us money grrr
+    * @author Thomas
+    */
     public Till(Restaurant rest, Customer c, Order order){
         this.rest = rest;
         this.order = order;
         this.c = c;
         this.amountDue = order.getTotal();
     }
-/**
- * processes a payment from the customer for the order.
- * @return returns the change due to the customer if any
- * @author Thomas
- */
+    /**
+    * processes a payment from the customer for the order.
+    * @return returns the change due to the customer if any
+    * @author Thomas
+    */
     public double processPayment(){
         System.out.println("1) Cash or 2) Card");
         int choice = in.nextInt();
         System.out.println("Enter amount given ");
         amountGiven = in.nextInt();
-
+        
         if(choice == 1 && amountGiven > amountDue){
             cash = true;
             cashInDrawer = cashInDrawer+amountDue;
         }else if(amountGiven > amountDue){
             creditCardT = creditCardT+amountDue;
+            change = 0;
         }
-        double change = 0;
+        
         if(cash && amountGiven>amountDue){
             change = amountGiven - amountDue;
         }
         Bill bill = new Bill(order);
         System.out.println(bill.toString());
+        System.out.println("Give"+ getChange() );
         return change;
-
+        
     }
-/**
- * 
- * @return gets Amount due which is how much the customer owes us
- * @author Thomas
- */
+    /**
+    * 
+    * @return gets Amount due which is how much the customer owes us
+    * @author Thomas
+    */
     public double getAmountDue() {
         return amountDue;
     }
-/**
- * 
- * @return this is how much the customer has given us so far
- * @author Thomas
- */
+    /**
+    * getting the change 
+    * @return change
+    * @author Thomas
+    */
+    public double getChange() {
+        return change;
+    }
+    /**
+    * 
+    * @return this is how much the customer has given us so far
+    * @author Thomas
+    */
     public int getAmountGiven() {
         return amountGiven;
     }
-/**
- * 
- * @return amount in the drawer currently
- * @author Thomas
- */
+    /**
+    * 
+    * @return amount in the drawer currently
+    * @author Thomas
+    */
     public double getCashInDrawer() {
         return cashInDrawer;
     }
-/**
- * 
- * @return How much we have been payed with credit cards
- * @author Thomas 
- */
+    /**
+    * 
+    * @return How much we have been payed with credit cards
+    * @author Thomas 
+    */
     public double getCreditCardT() {
         return creditCardT;
     }
-/**
- * 
- * @return gets the customers order
- * @author Thomas
- */
+    /**
+    * 
+    * @return gets the customers order
+    * @author Thomas
+    */
     public Order getOrder() {
         return order;
     }
-/**
- * 
- * @return gets the Restaurant
- * @author Thomas
- */
+    /**
+    * 
+    * @return gets the Restaurant
+    * @author Thomas
+    */
     public Restaurant getRest() {
         return rest;
     }
-
-   
+    
+    
 }
