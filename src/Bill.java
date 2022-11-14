f12f7683ac22b20cfcbdb2efcc82770dfcf17fcd
bourkupine <euanbourke@gmail.com>
2022-11-14 13:31:36 +0000

    added javadoc for Bill

--- src/Bill.java
+++ src/Bill.java
@@ -5,22 +5,39 @@ public class Bill{
     private String receipt ;
     private Order order;
 
+    /**
+     * Bill constructor
+     * @param order the order the Bill is for
+     * @author Thomas
+     */
     public Bill(Order order){//thomas bill will take an object order
        order.getCustomer().incrementLoyalty(); // incrementing the loyalty as they have requested the bill and are about to pay
        this.order=order;
        Receipt r =new Receipt(order.getR(),order);
     }
-    
 
+    /**
+     * receipt getter
+     * @return receipt as a String
+     */
     public String getReceipt() {
         return receipt;
     }
 
+    /**
+     * Receipt setter
+     * @param receipt set receipt to a String
+     * @author Thomas
+     */
     public void setReceipt(String receipt) {
         this.receipt = receipt;
     }
 
 
+    /**
+     * prints the bill
+     * @return returns the bill as a formatted String
+     */
     @Override
     public String toString() { //printing the bill
         return  String.format("Total  €%.2f. Thank you for visiting Yum", order.getTotal());
