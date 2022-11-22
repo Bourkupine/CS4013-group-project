496f7a53a308f2562058ad04a41936837907c488
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-22 12:27:09 +0000

    till fixing

--- src/Till.java
+++ src/Till.java
@@ -1,3 +1,4 @@
+import java.text.BreakIterator;
 import java.util.Scanner;
 //what else does a till do 
 //prints bill
@@ -7,77 +8,83 @@ import java.util.Scanner;
 * this is the till class
 * @author Thomas
 */
-public class Till {
-    Scanner in = new Scanner(System.in);
-    private double change = 0;
-    private Restaurant rest;
-    private boolean cash;
+public class Till implements ReadWrite {
     
-    private Order order;
-    private double creditCardT;
-    private double cashInDrawer;
-
-    /**
-    * 
-    * @param rest this is the restaurant the till is in
-    * @author Thomas
-    */
-    public Till(Restaurant rest, Order order){
-        this.rest = rest;//this is the restaurant
-        this.order = order;//this is the order
+    private Restaurant rest;
+    private double change;
+    private double amountGiven;
+    
+    public Till(Restaurant rest) {
+        this.rest = rest;
     }
     /**
     * processes a payment from the customer for the order.
     * @return returns the change due to the customer if any
     * @author Thomas
     */
-
-    //TODO is there really a point of returning change if we are going to tell them how much to give,
-    //also method is called but returned type is never used
-    public double processPayment(){
+    
+   
+    public void processPayment(Order order){
+        Scanner in = new Scanner(System.in);
+        change = 0;
+        amountGiven = 0;
         
         Bill bill = new Bill(order);//this creates a bill for said order
         System.out.println( order.getTotal() + " is the total");
         double amountDue = order.getTotal();
         double tip;
         double totalTips = 0;
-        System.out.println("Would you like to tip ?");
+        double total = order.getTotal();
+        System.out.println("Is the customer tipping ");
         System.out.println("1)Yes  2)No");
         int YN = in.nextInt();
         if(YN == 1  ){
-            System.out.println("How much would you like to tip?");
+            System.out.println("Enter tip amount");
             tip = in.nextInt();
             totalTips = totalTips + tip; 
-            double total = order.getTotal() + tip ;
+            total = order.getTotal() + tip ;
             System.out.println( total + ": is the total");
             System.out.println("You tipped :" + tip);
-
-
-        }else{ tip = 0;}
+            
+            
+        }else{ 
+            tip = 0;
+            System.out.println( total + ": is the total");
+        }
         
         System.out.println("1) Cash or 2) Card");//is the customer paying in cash or card
         int choice = in.nextInt(); //they decide what they pay with
-        System.out.println("Enter amount given "); //this is the amount the customer gives
-        int amountGiven = in.nextInt();
-        System.out.println(bill.toString());
         
+        double amountGiven = 0;
         
-        if(choice == 1 && amountGiven >= amountDue){
-            cash = true;
-            cashInDrawer = cashInDrawer+amountDue;
-        }else if(amountGiven < amountDue){
-            creditCardT = creditCardT+amountDue;
-            change = 0;
-            System.out.printf("Give %f", change);
+        while(amountGiven < amountDue){
+            System.out.println(amountDue +" left to pay");
+            System.out.println("Enter amount given "); //this is the amount the customer gives
+            amountGiven += in.nextInt();
         }
         
-        if(cash && amountGiven>amountDue){
-            change = amountGiven - amountDue;
+        if(amountGiven> amountDue){
+            System.out.println(change + " is the change");
         }
+        printReceipt(order);
+        updateFile(rest.getMoney(), toCsv(order));
         rest.removeOrder(order);
-        System.out.println(bill.toString());
-        return change;
         
+    }
+    /**
+    * prints the receipt
+    * @return returns the receipt as a formatted String
+    */
+    private String printReceipt(Order order) { 
+        return  
+        String.format(order.toString()+ "\n"
+        + "Amount given = " + amountGiven + "\n"
+        + "Change = " + change + "\n"
+        + "Total  €%.2f. Thank you for visiting Yum", order.getTotal());
+    }
+    
+    private String toCsv(Order order){
+        return order.getDate().toString()+","+order.getTotal();
     }
     
 }
